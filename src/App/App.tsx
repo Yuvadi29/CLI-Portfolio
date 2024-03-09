6956c5f8cc670c5b45dd18aaf9db26efd8bc4430
Yuvadi29 <adityatrivedi612@gmail.com>
2024-03-09 15:28:06 +0530

    Started with Terminal Design

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from "./App.module.scss";
+
+const App = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.window}>
+        Hello World
+      </div>
+    </div>
+  )
+}
+
+export default App;
